88f3c4bc63667a9110cd6aba0a2ed6f8a1596cf1
= <steven.serruya@gmail.com>
2023-09-21 20:13:55 -0500

    fix admin basic auth

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate
 
   def index
     @products = Product.order(id: :desc).all
@@ -36,5 +37,9 @@ class Admin::ProductsController < ApplicationController
       :price
     )
   end
-
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
 end
