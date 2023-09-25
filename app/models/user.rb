45a2288d72ed7a0fe4c4656ba034574c3427ae12
= <steven.serruya@gmail.com>
2023-09-25 18:59:56 -0500

    Password minimum length and New authentication (class) method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }, if: :password_present?
+
   validates :first_name, presence: true
   validates :last_name, presence: true
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email = email.downcase if email.present?
+  end
+
+  def password_present?
+    password.present?
+  end
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.where('LOWER(email) = ?', email.downcase.strip).first
+    user && user.authenticate(password) ? user : nil
+end
+
+
+  
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
@@ -42,4 +41,63 @@ end
     end
 
   end
+
+  # Test section for password length requirements
+describe 'Password length validations' do
+
+  # Test to ensure that passwords shorter than 8 characters are not valid
+  it 'is not valid with a password shorter than 8 characters' do
+    user = User.new(email: 'test@test.com', password: 'short', password_confirmation: 'short', first_name: 'John', last_name: 'Doe')
+    expect(user).to_not be_valid
+    expect(user.errors.full_messages).to include("Password is too short (minimum is 8 characters)")
+  end
+
+  # Test to confirm that passwords of 8 characters or more are valid
+  it 'is valid with a password of 8 characters or more' do
+    user = User.new(email: 'test@test.com', password: 'longpassword', password_confirmation: 'longpassword', first_name: 'John', last_name: 'Doe')
+    expect(user).to be_valid
+  end
+end
+
+# Test section for the custom authentication method
+describe '.authenticate_with_credentials' do
+  
+  # Setup a sample user for authentication tests
+  before do
+    @user = User.create(email: 'test@test.com', password: 'password123', password_confirmation: 'password123', first_name: 'John', last_name: 'Doe')
+  end
+
+  # Test for successful authentication with correct credentials
+  it 'authenticates valid credentials' do
+    expect(User.authenticate_with_credentials('test@test.com', 'password123')).to eq(@user)
+  end
+
+  # Test for unsuccessful authentication with incorrect password
+  it 'does not authenticate invalid credentials' do
+    expect(User.authenticate_with_credentials('test@test.com', 'wrongpassword')).to be_nil
+  end
+
+  # Test for successful authentication even if email has leading/trailing whitespaces
+  it 'authenticates with leading or trailing whitespaces in email' do
+    expect(User.authenticate_with_credentials('  test@test.com  ', 'password123')).to eq(@user)
+  end
+
+  # Test for successful authentication with email in mixed case
+  it 'authenticates with mixed case email' do
+    expect(User.authenticate_with_credentials('TeSt@tEst.com', 'password123')).to eq(@user)
+  end
+
+  # Redundant test (as the above already covers this scenario), but keeping it for completeness
+  it 'authenticates email with leading and trailing spaces' do
+    authenticated_user = User.authenticate_with_credentials('  test@test.com  ', 'password123')
+    expect(authenticated_user).to eq(@user)
+  end
+
+  # Another test for successful authentication with email in different cases
+  it 'authenticates email with different case' do
+    authenticated_user = User.authenticate_with_credentials('TeSt@tEst.COM', 'password123')
+    expect(authenticated_user).to eq(@user)
+  end
+end
+
 end
