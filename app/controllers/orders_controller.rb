0a643a5290ab9275c8c2730a2d33816aa1b5a382
= <steven.serruya@gmail.com>
2023-09-26 16:22:38 -0500

    added some logic to the cart. cant buy more than stock

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -51,7 +51,14 @@ class OrdersController < ApplicationController
         item_price: product.price,
         total_price: product.price * quantity
       )
+      product.quantity -= quantity
+      product.save!
     end
+
+  
+
+  
+
     order.save!
     order
   end
